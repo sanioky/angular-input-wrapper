e08004c517d9b32c181380f3ee2286ab7cf0638a
sanioky <sanioky@gmail.com>
2023-04-26 17:06:08 +0300

    init ax-control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
+import { AxControlComponent } from './components/ax-control/ax-control.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        FormComponent
+        FormComponent,
+        AxControlComponent
     ],
     imports: [
         BrowserModule,
